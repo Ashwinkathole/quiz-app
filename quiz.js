5725f7bf2cbc343a108c459b9b5fcf197795ab55
ashwinkathole2001@gmail.com <ashwinkathole2001@gmail.com>
2025-01-04 03:18:12 -0800

    new add

--- quiz.js
+++ quiz.js
@@ -1,2 +1,36 @@
 
-    
+function signup() {
+    const name = document.getElementById("fullname").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    const namePattern = /^[a-zA-Z]+([ '-][a-zA-Z]+)*$/;
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const passwordPattern=/^(?=.[A-Z])(?=.[a-z])(?=.\d)(?=.[@$!%?&])[A-Za-z\d@$!%?&]{8,}$/;
+
+    if (!name) {
+        alert("please enter your full name");
+        return;
+    }
+    if (!namePattern.test(name)) {
+        alert("please enter a valid name");
+        return;
+    }
+    if (!email) {
+        alert("please enter a email");
+        return;
+    }
+    if (!emailPattern.test(email)) {
+        alert("please enter valid email ");
+        return;
+    }
+    if(!password){
+        alert("please enter password");
+        return;
+    }
+    if(!passwordPattern.test(password)){
+        alert("please enter valid password");
+        return;
+    }
+}
+
