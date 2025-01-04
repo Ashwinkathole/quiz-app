e8d879193b43a848d35465aa4b6fc5d4ba83ed84
ashwinkathole2001@gmail.com <ashwinkathole2001@gmail.com>
2025-01-04 01:36:44 -0800

    new add

--- quiz.js
+++ quiz.js
@@ -1,15 +1,2 @@
-function login(){
-    var email=document.getElementById("email").value;
-    var password=document.getElementById("password").value;
 
-    if(email=== '') {
-        alert("email is required");
-        return;
-    }
-    if(password==='') {
-        alert("password is required");
-        return;
-    }
-    alert("login successful!");
-    }
     
